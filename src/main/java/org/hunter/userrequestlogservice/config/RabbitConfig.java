39f2fa24a03ca2078cf3b014186d4fc671ff16e7
Hunter Stern <vonrosen2000@yahoo.com>
2020-01-12 13:24:07 -0800

    more changes

--- src/main/java/org/hunter/userrequestlogservice/config/RabbitConfig.java
+++ src/main/java/org/hunter/userrequestlogservice/config/RabbitConfig.java
@@ -1,5 +1,8 @@
 package org.hunter.userrequestlogservice.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -11,26 +14,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
 
-	@Bean
-	public TopicExchange appExchange() {
-		return new TopicExchange("loan");
-	}
+    @Bean
+    public TopicExchange appExchange() {
+        return new TopicExchange("loan");
+    }
 
-	@Bean
-	public Queue queue() {
-		return new Queue("userrequestlog");
-	}
+    @Bean
+    public Queue queue() {
+        Map<String, Object> args = new HashMap<String, Object>();
+        args.put("x-message-ttl", 300000);
+        return new Queue("userrequestlog", true, false, false, args);
+    }
 
-	@Bean
-	public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-		rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
-		return rabbitTemplate;
-	}
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
 
-	@Bean
-	public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-		return new Jackson2JsonMessageConverter();
-	}
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
 }
